e293b15b0535943b47acee0ea1096a17c0e54581
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-02-02 01:06:33 +0100

    Section 15 - Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module fx.masterclass.helloworldfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+//    opens java.masterclass.helloworldfx to javafx.fxml;
+//    exports java.masterclass.helloworldfx
+
+    opens fx.masterclass.helloworldfx;
+
+
+}
