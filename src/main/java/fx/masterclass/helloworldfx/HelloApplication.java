6bbca9bd7a249d1268e9dcb05a81b207f7901279
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-02-02 01:41:07 +0100

    Section 15 - JavaFX hello world program

--- src/main/java/fx/masterclass/helloworldfx/HelloApplication.java
+++ src/main/java/fx/masterclass/helloworldfx/HelloApplication.java
@@ -11,8 +11,20 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//        GridPane root= new GridPane();
+//        root.setAlignment(Pos.CENTER);
+//        root.setVgap(10);
+//        root.setHgap(10);
+//
+//        Label greetings = new Label("Welcome to JavaFX!");
+//        greetings.setTextFill(Color.GREEN);
+//        greetings.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
+//
+//        root.getChildren().add(greetings);
+
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello World!!!");
+//        Scene scene = new Scene(root, 700, 240);
+        stage.setTitle("Hello JavaFX!");
         stage.setScene(scene);
         stage.show();
     }
