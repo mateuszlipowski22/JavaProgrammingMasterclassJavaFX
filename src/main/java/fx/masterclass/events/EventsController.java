8140232a9c2d5f6f0f04fdfb2b1883851404bc22
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-02-06 21:10:33 +0100

    Section 15 - events and events handlers #2

--- src/main/java/fx/masterclass/events/EventsController.java
+++ src/main/java/fx/masterclass/events/EventsController.java
@@ -1,16 +1,41 @@
 package fx.masterclass.events;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class EventsController {
 
     @FXML
     private TextField nameField;
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Button byeButton;
 
     @FXML
-    public void onButtonClicked(){
-        System.out.println("Hello " + nameField.getText());
+    public void initialize(){
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+    }
+
+    @FXML
+    public void onButtonClicked(ActionEvent e){
+     if(e.getSource().equals(helloButton)){
+         System.out.println("Hello, "+nameField.getText());
+     }else if(e.getSource().equals(byeButton)){
+         System.out.println("Bye, "+nameField.getText());
+     }
+    }
+
+    @FXML
+    public void handleKeyRelease(){
+        String text = nameField.getText();
+        boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
+        helloButton.setDisable(disableButtons);
+        byeButton.setDisable(disableButtons);
+
     }
 
 }
