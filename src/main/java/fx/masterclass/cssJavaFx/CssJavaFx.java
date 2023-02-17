325c0b181cf2b8b89a8c410a897e86cfa73666d6
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-02-18 00:59:21 +0100

    Section 15 - chooser

--- src/main/java/fx/masterclass/cssJavaFx/CssJavaFx.java
+++ src/main/java/fx/masterclass/cssJavaFx/CssJavaFx.java
@@ -12,7 +12,7 @@ public class CssJavaFx extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(fx.masterclass.cssJavaFx.CssJavaFx.class.getResource("cssJavaFx-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 275);
-        setUserAgentStylesheet(STYLESHEET_CASPIAN);
+//        setUserAgentStylesheet(STYLESHEET_CASPIAN);
         stage.setTitle("Hello World");
         stage.setScene(scene);
         stage.show();
--- src/main/java/fx/masterclass/cssJavaFx/CssJavaFxController.java
+++ src/main/java/fx/masterclass/cssJavaFx/CssJavaFxController.java
@@ -1,4 +1,50 @@
 package fx.masterclass.cssJavaFx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
+
+import java.io.File;
+
 public class CssJavaFxController {
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private Button button4;
+
+    @FXML
+    private GridPane gridPane;
+
+    public void initialize(){
+        button4.setEffect(new DropShadow());
+    }
+
+    @FXML
+    public void handleMouseEnter(){
+        label.setScaleY(2.0);
+        label.setScaleX(2.0);
+    }
+
+    @FXML
+    public void handleMouseExit(){
+        label.setScaleY(1.0);
+        label.setScaleX(1.0);
+    }
+
+    @FXML
+    public void handleClick(){
+//        FileChooser chooser = new FileChooser();
+        DirectoryChooser chooser = new DirectoryChooser();
+        File file = chooser.showDialog(gridPane.getScene().getWindow());
+        if(file!=null){
+            System.out.println(file.getPath());
+        }else {
+            System.out.println("Chooser was cancelled");
+        }
+    }
 }
