a4c395ba297cbe025d3341cc12ed7aa6ddfa6c39
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-02-21 00:21:42 +0100

    Section 15 - add contact controller and main-view update

---
+++ src/main/java/fx/masterclass/myContacts/ContactController.java
@@ -0,0 +1,4 @@
+package fx.masterclass.myContacts;
+
+public class ContactController {
+}
--- src/main/java/fx/masterclass/myContacts/Controller.java
+++ src/main/java/fx/masterclass/myContacts/Controller.java
@@ -1,4 +1,12 @@
 package fx.masterclass.myContacts;
 
+import javafx.fxml.FXML;
+
 public class Controller {
+
+    @FXML
+    public void showAddContactDialog(){
+
+    }
+
 }
