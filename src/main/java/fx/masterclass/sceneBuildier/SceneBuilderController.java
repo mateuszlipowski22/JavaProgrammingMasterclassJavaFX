badb53b62af6c7daafa2009e4bff8befb213f651
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-02-20 21:55:24 +0100

    Section 15 - scene builder #2

--- src/main/java/fx/masterclass/sceneBuildier/SceneBuilderController.java
+++ src/main/java/fx/masterclass/sceneBuildier/SceneBuilderController.java
@@ -1,6 +1,15 @@
 package fx.masterclass.sceneBuildier;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class SceneBuilderController {
 
+    @FXML
+    private Label label;
 
+    @FXML
+    private void handleAction(){
+        label.setText("OK button pressed");
+    }
 }
