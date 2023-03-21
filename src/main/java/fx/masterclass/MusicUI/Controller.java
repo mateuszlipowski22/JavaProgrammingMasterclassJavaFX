a43d70ae8df48fd32d1709308987f258ed15a6cf
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-03-22 00:57:21 +0100

    Section 22 - add artists

--- src/main/java/fx/masterclass/MusicUI/Controller.java
+++ src/main/java/fx/masterclass/MusicUI/Controller.java
@@ -1,4 +1,28 @@
 package fx.masterclass.MusicUI;
 
+import fx.masterclass.MusicUI.model.Artist;
+import fx.masterclass.MusicUI.model.Datasource;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
 public class Controller {
+
+    @FXML
+    private TableView<Artist> artistTable;
+
+    public void listArtist(){
+        Task<ObservableList<Artist>> task = new GetAllArtistsTask();
+        artistTable.itemsProperty().bind(task.valueProperty());
+    }
+
+}
+
+class GetAllArtistsTask extends Task{
+    @Override
+    protected ObservableList<Artist> call() throws Exception {
+        return FXCollections.observableArrayList(Datasource.getInstance().queryArtists(Datasource.ORDER_BY_ASC));
+    }
 }
